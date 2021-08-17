2a2a1ba8c0c9c77e3e4bd4af8b84b4043cf9d87e
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-08-17 17:28:19 +0300

    solution problem 1

---
+++ src/test/java/Solution1Test.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution1Test  {
+    Solution1 solution1 = new Solution1();
+
+    @Test
+    public void twoSumReturnZeroAndOneWhenSummFirstAndSecondEqualsTarget() {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] expected = new int[]{0,1};
+
+        int[] result = solution1.twoSum(nums, target);
+
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+    @Test
+    public void twoSumReturnZeroAndOneWhenSummFirstAndSecondEqualsTargetNegativeNumbers() {
+        int[] nums = new int[]{-2,-7,11,15};
+        int target = -9;
+        int[] expected = new int[]{0,1};
+
+        int[] result = solution1.twoSum(nums, target);
+
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+    @Test
+    public void twoSumReturnZeroAndtwoWhenSummFirstAndThirdEqualsTargetNegativeNumbers() {
+        int[] nums = new int[]{-2,-7,11,15};
+        int target = 9;
+        int[] expected = new int[]{0,2};
+
+        int[] result = solution1.twoSum(nums, target);
+
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+    }
+
+}
